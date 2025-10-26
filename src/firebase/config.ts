ea6a290bad02d72ef9ae39a0093b3a541155ecde
Monirul Hasan talha <monirulhasantalha96@gmail.com>
2025-10-26 03:37:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1934459952-7564a",
+  "appId": "1:1002785289034:web:e984f686b661cdbf0907b3",
+  "apiKey": "AIzaSyAamHfL3FPEzzOycPAWbtZti0AEX648Jkc",
+  "authDomain": "studio-1934459952-7564a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1002785289034"
+};
